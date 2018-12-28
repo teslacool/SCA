4c44c871fa551da2869d8c394831eab4f6f8d6bb
Tesla <1462540095@qq.com>
2018-12-28 03:12:12 +0000

    add two schedule

---
+++ fairseq/lmoutschedule/squareschedule.py
@@ -0,0 +1,39 @@
+from torch import nn
+
+from fairseq import utils
+from . import register_lmoutschedule
+
+@register_lmoutschedule('square')
+class SquareLmOutSchedule(object):
+
+    def __init__(self, args, encoder):
+        warmup_init_tradeoff = args.tradeoff
+        warmup_end_tradeoff = args.tradeoff
+        # linearly warmup for the first args.warmup_updates
+        self.lr_step = (warmup_end_tradeoff - warmup_init_tradeoff) / args.tradeoff_step
+        self.warmup_init_tradeoff = warmup_init_tradeoff
+        # then, decay prop. to the inverse square root of the update number
+        self.decay_factor = warmup_end_tradeoff * args.tradeoff_step ** 2
+        self.tradeoff_step = args.tradeoff_step
+
+        # initial learning rate
+        self.tradeoff = warmup_init_tradeoff
+        self.encoder = encoder
+        self.set_tradeoff()
+
+    @staticmethod
+    def add_args(parser):
+        parser.add_argument('--tradeoff', type=float, default=1.)
+        parser.add_argument('--tradeoff-step', type=int, default=4000)
+
+    def set_tradeoff(self):
+        self.encoder.tradeoff = self.tradeoff
+
+    def step_update(self, num_updates):
+        """Update the learning rate after each update."""
+        if num_updates <= self.tradeoff_step:
+            self.tradeoff = self.warmup_init_tradeoff + self.lr_step * num_updates
+        else:
+            self.tradeoff = self.decay_factor * num_updates ** -2.
+        self.set_tradeoff()
+        return self.tradeoff
