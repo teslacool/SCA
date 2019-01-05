0225885fc30dba45e9ae0de07bce5f7dcc1cf8d2
Tesla <1462540095@qq.com>
2019-01-05 02:11:13 +0000

    tradeoff limit

--- fairseq/lmoutschedule/linearschedule.py
+++ fairseq/lmoutschedule/linearschedule.py
@@ -1,5 +1,4 @@
 from torch import nn
-import numpy as np
 from fairseq import utils
 from . import register_lmoutschedule
 
@@ -20,16 +19,27 @@ class LinearLmOutSchedule(object):
         self.tradeoff = warmup_init_tradeoff
         self.encoder = encoder
         self.decoder = decoder
+        self.upper_limit = args.lm_upper_limit
+        self.lower_limit = args.lm_lower_limit
+        assert self.upper_limit > self.lower_limit
         self.set_tradeoff()
 
     @staticmethod
     def add_args(parser):
         parser.add_argument('--tradeoff', type=float, default=1.)
         parser.add_argument('--tradeoff-step', type=int, default=4000)
+        parser.add_argument('--lm-upper-limit', type=float, default=1.0)
+        parser.add_argument('--lm-lower-limit', type=float, default=0.1)
 
     def set_tradeoff(self):
         self.encoder.tradeoff = self.tradeoff
         self.decoder.tradeoff = self.tradeoff
+
+    def clip(self):
+        if self.tradeoff > self.upper_limit:
+            self.tradeoff = self.upper_limit
+        elif self.tradeoff < self.lower_limit:
+            self.tradeoff = self.lower_limit
 
     def step_update(self, num_updates):
         """Update the learning rate after each update."""
@@ -37,6 +47,6 @@ class LinearLmOutSchedule(object):
             self.tradeoff = self.warmup_init_tradeoff + self.lr_step * num_updates
         else:
             self.tradeoff = self.decay_factor / num_updates
-        self.tradeoff = float(np.clip(self.tradeoff, 0.1, 1.))
+        self.clip()
         self.set_tradeoff()
         return self.tradeoff
